94deb8a94a9b8708b2a5e0e8d588951cc42ec432
jer-keel <jerkeeler@gmail.com>
2016-08-12 09:11:37 -0400

    Create models

--- translator/models.py
+++ translator/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class TranslatedText(models.Model):
+    original_text = models.TextField()
+    translated_text = models.TextField()
+    translate_date = models.DateTimeField('date translated')
+    detected_langugae_code = models.CharField(max_length=10)
+    detected_language = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.original_text
