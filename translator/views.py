159e902393d9c4ee2b2470e7326c39847b7da4f5
jer-keel <jerkeeler@gmail.com>
2016-08-12 10:26:25 -0400

    Create post request from form and detect language

--- translator/views.py
+++ translator/views.py
@@ -1,14 +1,28 @@
 from django.shortcuts import render, get_list_or_404, get_object_or_404
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse, HttpResponseRedirect, Http404, JsonResponse
+from django.utils.html import escape
+from langdetect import detect
 
 from .models import TranslatedText
-# Create your views here.
 
 
 def index(request):
-    return render(request, 'translator/index.html')
+    return render(request, "translator/index.html")
 
 
 def texts(request):
     texts = get_list_or_404(TranslatedText)
-    return render(request, 'translator/texts.html', {'texts': texts})
+    return render(request, "translator/texts.html", {"texts": texts})
+
+
+def translate(request):
+    if request.method == "POST":
+        input_string = request.POST.get("input_string")
+        if input_string is None:
+            return Http404("That's not allowed!")
+        else:
+            input_string = escape(input_string)
+            lang = detect(input_string)
+            return HttpResponse(lang)
+    else:
+        return HttpResponseRedirect("/texts")
