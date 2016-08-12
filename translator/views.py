4206eef67eeb749afcb8b6b1861b6425553ac4e4
jer-keel <jerkeeler@gmail.com>
2016-08-12 09:11:42 -0400

    Create views

--- translator/views.py
+++ translator/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404, get_object_or_404
+from django.http import HttpResponse, JsonResponse
 
+from .models import TranslatedText
 # Create your views here.
+
+
+def index(request):
+    return render(request, 'translator/index.html')
+
+
+def texts(request):
+    texts = get_list_or_404(TranslatedText)
+    return render(request, 'translator/texts.html', {'texts': texts})
