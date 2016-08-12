a4bab126ef2775415efaa3ccbe986e29967594e0
jer-keel <jerkeeler@gmail.com>
2016-08-12 11:09:30 -0400

    Add translation position and save to database

--- translator/views.py
+++ translator/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render, get_list_or_404, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect, Http404, JsonResponse
 from django.utils.html import escape
+from datetime import datetime
 from langdetect import detect
 
+from googleapiclient.discovery import build
+import json
+
 from .models import TranslatedText
+
+API_TOKEN = json.load(open("translator/config/google_api.json"))["API_TOKEN"]
+service = build("translate", "v2", developerKey=API_TOKEN)
 
 
 def index(request):
@@ -23,6 +30,20 @@ def translate(request):
         else:
             input_string = escape(input_string)
             lang = detect(input_string)
-            return HttpResponse(lang)
+            print(lang)
+            translated_text = service.translations().list(
+                source=lang,
+                target="en",
+                q=[input_string]
+            ).execute()["translations"][0]["translatedText"]
+
+            text = TranslatedText()
+            text.original_text = input_string
+            text.translated_text = translated_text
+            text.translate_date = datetime.now()
+            text.detected_langugae_code = lang
+            text.detected_language = lang
+            text.save()
+            return HttpResponse(translated_text)
     else:
         return HttpResponseRedirect("/texts")
